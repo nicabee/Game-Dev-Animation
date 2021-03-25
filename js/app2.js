d99d5f9369595a46d05e92ac842cb2a2bf514ba0
Monica <monicabee358@gmail.com>
2021-03-25 19:32:25 +0800

    managit add -A

--- js/app2.js
+++ js/app2.js
@@ -34,158 +34,127 @@ canvas.onkeyup = function(e) {
 var image = new Image();
 
 image.onload = function() {
-    sprChar = new Character({
+    sprChar = new player({
         gameWidth: canvas.width,
         gameHeight: canvas.height,
         image: image,
         spritesheetWidth: 32,
         spritesheetHeight: 48,
-        interval: 100,
-        left: 10,
-        top: 10,
+        secondsPerFrame: 50, //framespersecond
+        x: 10, // initial location of sprite, x
+        y: 10, // initial location of sprite, y
     });
 
     sprChar.addSprite({
-        name: "down",
+        directionname: "down",
         start: 0,
         frames: 4
     });
 
     sprChar.addSprite({
-        name: "left",
+        directionname: "left",
         start: 4,
         frames: 4
     });
     sprChar.addSprite({
-        name: "right",
+        directionname: "right",
         start: 8,
         frames: 4
     });
     sprChar.addSprite({
-        name: "up",
+        directionname: "up",
         start: 12,
         frames: 4
     });
     sprChar.addSprite({
-        name: "idle",
+        directionname: "idle",
         start: 16,
         frames: 4
     });
     sprChar.setSprite("idle");
     setInterval(function() {
-        sprChar.update();
-        canvas.width = canvas.width;
+        sprChar.animate();
+        canvas.width = canvas.width; //erase traces of previous sprite animations
         sprChar.draw(ctx);
     }, 1000 / 60);
 };
 
 image.src = 'img/test3.png';
 
-// CHARACTER
-
-function Character(data) {
-
-    this.gameWidth = data.gameWidth;
-    this.gameHeight = data.gameHeight;
-
+function player(zeldaSkeleton) {
+    this.gameWidth = zeldaSkeleton.gameWidth;
+    this.gameHeight = zeldaSkeleton.gameHeight;
     this.xcoord = 0;
     this.ycoord = 0;
-
-    this.init(data);
+    this.startSprite(zeldaSkeleton);
 }
 
 
-// ANIMATED SPRITE
-var AnimatedSprite = function(data) {
-    this.init(data);
-    this.isFinished = false;
+// initiate
+var spriteToBeUsed = function(zeldaSkeleton) {
+    this.startSprite(zeldaSkeleton);
     this.currentSprite = null;
     this.currentFrame = 0;
-    this.lastTick = 0;
+    this.oldSeconds = 0;
 };
 
-AnimatedSprite.prototype = {
-    start: function(){
-    // this.isFinished = false;
-    this.currentFrame = 0;
-    },        
-    init: function(data) {
-        if (data) {
-
-            this.isLooping = data.isLooping;
+ spriteToBeUsed.prototype = { //use prototype to attach new properties (needed as animation changes as key is pressed)    
+    startSprite: function(zeldaSkeleton) {
+        if (zeldaSkeleton) {
+            this.isLooping = zeldaSkeleton.isLooping;
             if (typeof this.isLooping != "boolean") this.isLooping = true;
-
-            this.image = data.image;
-            this.spritesheetWidth = data.spritesheetWidth;
-            this.spritesheetHeight = data.spritesheetHeight || this.spritesheetWidth;
-
+            this.image = zeldaSkeleton.image;
+            this.spritesheetWidth = zeldaSkeleton.spritesheetWidth;
+            this.spritesheetHeight = zeldaSkeleton.spritesheetHeight;
             this.sprites = [];
-            this.interval = data.interval;
-
-            this.left = data.left;
-            this.top = data.top;
-            this.width = data.width || this.spritesheetWidth;
-            this.height = data.hegiht || this.spritesheetHeight;
-
-            this.onCompleted = data.onCompleted;
+            this.secondsPerFrame = zeldaSkeleton.secondsPerFrame;
+            this.x = zeldaSkeleton.x;
+            this.y = zeldaSkeleton.y;
+            this.width = this.spritesheetWidth;
+            this.height = this.spritesheetHeight;
         }
     },
-    addSprite: function(data) {
-        this.sprites[data.name] = {
-            name: data.name,
-            start: data.start || 0,
-            frames: data.frames || 1,
-            framesPerRow: Math.floor(this.image.width / this.spritesheetWidth)
+    addSprite: function(zeldaSkeleton) {
+        this.sprites[zeldaSkeleton.directionname] = {
+            directionname: zeldaSkeleton.directionname,
+            start: zeldaSkeleton.start || 0,
+            frames: zeldaSkeleton.frames || 1,
+            framesPerRow: Math.floor(this.image.width / this.spritesheetWidth) //divide the width per frame for every single animation
         };
-
-        this.currentSprite = this.currentSprite || this.sprites[data.name];
+        this.currentSprite = this.currentSprite || this.sprites[zeldaSkeleton.directionname];
     },
-    setSprite: function(name) {
-        if (this.currentSprite != this.sprites[name]) {
-            this.currentSprite = this.sprites[name];
+    setSprite: function(directionname) { //setSprite based on current movement (idle/not)
+        if (this.currentSprite != this.sprites[directionname]) {
+            this.currentSprite = this.sprites[directionname];
             this.currentFrame = 0;
         }
     },
-    update: function() {
-        if (this.isFinished) return;
-        var newTick = (new Date()).getTime();
-        if (newTick - this.lastTick >= this.interval) {
+    animate: function() {
+        var newSeconds = (new Date()).getTime();
+        if (newSeconds - this.oldSeconds >= this.secondsPerFrame) {
             this.currentFrame++;
-
             if (this.currentFrame == this.currentSprite.frames) {
-                if (this.isLooping) this.currentFrame = 0;
-                else {
-                    this.isFinished = true;
-                    if (this.onCompleted) this.onCompleted();
-                }
-
+                if (this.isLooping) this.currentFrame = 0; //show current animation frame
             }
-            this.lastTick = newTick;
+            this.oldSeconds = newSeconds;
         }
-
     },
     draw: function(ctx) {
-
-        if (this.isFinished) return;
-        var realIndex = this.currentSprite.start + this.currentFrame;
-        var row = Math.floor(realIndex / this.currentSprite.framesPerRow);
-        var col = realIndex % this.currentSprite.framesPerRow;
-
-        ctx.drawImage(this.image, col * this.spritesheetWidth, row * this.spritesheetHeight, this.spritesheetWidth, this.spritesheetHeight, this.left, this.top, this.width, this.height);
-
+        var locSprite = this.currentSprite.start + this.currentFrame; // current frame start + currentframe's start being executed
+        var r = Math.floor(locSprite / this.currentSprite.framesPerRow);
+        var c = locSprite % this.currentSprite.framesPerRow;
+        ctx.drawImage(this.image, c * this.spritesheetWidth, r * this.spritesheetHeight, this.spritesheetWidth, this.spritesheetHeight, this.x, this.y, this.width, this.height);
     }
 }
+player.prototype = new spriteToBeUsed();
+player.prototype.animate = function() {
 
-// CHARACTER
-Character.prototype = new AnimatedSprite();
-Character.prototype.update = function() {
+    var x = this.x + this.xcoord;
+    var y = this.y + this.ycoord;
+    var xplus = x + this.spritesheetWidth;
+    var yplus = y + this.spritesheetHeight;
 
-    var left = this.left + this.xcoord;
-    var top = this.top + this.ycoord;
-    var right = left + this.spritesheetWidth;
-    var bottom = top + this.spritesheetHeight;
-
-    if (left > 0 && right < this.gameWidth) this.left = left;
-    if (top > 0 && bottom < this.gameHeight) this.top = top;
-    AnimatedSprite.prototype.update.call(this);
+    if (x > 0 && xplus < this.gameWidth) this.x = x;
+    if (y > 0 && yplus < this.gameHeight) this.y = y;
+    spriteToBeUsed.prototype.animate.call(this);
 };
